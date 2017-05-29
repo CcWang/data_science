1455278f2188603ba07bc76befc7354667ffffbb
Chongchong Wang <chongchong1110@gmail.com>
2017-05-28 21:33:52 -0700

    finish question one get energy

--- intro/assignment3.py
+++ intro/assignment3.py
@@ -1,14 +1,9 @@
 import pandas as pd 
 import numpy as np
 import xlrd 
+import re
 #question one
 # Engergy Indicators 
-# 6. Rename the following list of countries (for use in later questions):
-#	"Republic of Korea": "South Korea",
-#	"United States of America": "United States",
-#	"United Kingdom of Great Britain and Northern Ireland": "United Kingdom",
-#	"China, Hong Kong Special Administrative Region": "Hong Kong"
-#	There are also several countries with numbers and/or parenthesis in their name. Be sure to remove these
 
 def get_energy():
 	#1 & 2. put Energy Indicators.xls into a DataFrame with the variable name of energy exclude the footer and the hader
@@ -27,13 +22,27 @@ def get_energy():
 	# 5. Convert Energy Supply to gigajoules (there are 1,000,000 gigajoules in a petajoule).
 	energy['Energy Supply'] *=1000000
 
+	# 6. Rename the following list of countries (for use in later questions):
+	#	"Republic of Korea": "South Korea",
+	#	"United States of America": "United States",
+	#	"United Kingdom of Great Britain and Northern Ireland": "United Kingdom",
+	#	"China, Hong Kong Special Administrative Region": "Hong Kong"
+	#	There are also several countries with numbers and/or parenthesis in their name. Be sure to remove these
+	
 	country_rename_map = {"Republic of Korea": "South Korea",
 							"United States of America20": "United States",
 							"United Kingdom of Great Britain and Northern Ireland": "United Kingdom",
 							"China, Hong Kong Special Administrative Region": "Hong Kong"}
 	energy.replace({'Country':country_rename_map}, inplace = True)
+	# remove_num_par = "(?P<{0}>[a-zA-Z\s\-]+)\b?(\(|\d)*"
+	energy['Country'] = energy['Country'].str.replace(r"[\(\)\d]+", "")
 
-	print energy
+	return energy
+
+#get GDP
+def get_GDP():
+	
+	
 
 get_energy()
 
