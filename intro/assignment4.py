e3b9d3c299feb7dd4d1d748746a55d13162acfe5
Chongchong Wang <chongchong1110@gmail.com>
2017-06-06 22:50:52 -0700

    get and clean housing data

--- intro/assignment4.py
+++ intro/assignment4.py
@@ -17,7 +17,6 @@ import operator
 
 #  step one: get all your data, make them clean and easy to use
 states = {'OH': 'Ohio', 'KY': 'Kentucky', 'AS': 'American Samoa', 'NV': 'Nevada', 'WY': 'Wyoming', 'NA': 'National', 'AL': 'Alabama', 'MD': 'Maryland', 'AK': 'Alaska', 'UT': 'Utah', 'OR': 'Oregon', 'MT': 'Montana', 'IL': 'Illinois', 'TN': 'Tennessee', 'DC': 'District of Columbia', 'VT': 'Vermont', 'ID': 'Idaho', 'AR': 'Arkansas', 'ME': 'Maine', 'WA': 'Washington', 'HI': 'Hawaii', 'WI': 'Wisconsin', 'MI': 'Michigan', 'IN': 'Indiana', 'NJ': 'New Jersey', 'AZ': 'Arizona', 'GU': 'Guam', 'MS': 'Mississippi', 'PR': 'Puerto Rico', 'NC': 'North Carolina', 'TX': 'Texas', 'SD': 'South Dakota', 'MP': 'Northern Mariana Islands', 'IA': 'Iowa', 'MO': 'Missouri', 'CT': 'Connecticut', 'WV': 'West Virginia', 'SC': 'South Carolina', 'LA': 'Louisiana', 'KS': 'Kansas', 'NY': 'New York', 'NE': 'Nebraska', 'OK': 'Oklahoma', 'FL': 'Florida', 'CA': 'California', 'CO': 'Colorado', 'PA': 'Pennsylvania', 'DE': 'Delaware', 'NM': 'New Mexico', 'RI': 'Rhode Island', 'MN': 'Minnesota', 'VI': 'Virgin Islands', 'NH': 'New Hampshire', 'MA': 'Massachusetts', 'GA': 'Georgia', 'ND': 'North Dakota', 'VA': 'Virginia'}
-city_home_source = 'City_Zhvi_AllHomes.csv'
 university_towns_source = 'university_towns.txt'
 gdp_source = 'gdplev.xls'
 chained_dollars = 'Quarterly GDP 2009 Billions'
@@ -94,11 +93,51 @@ def get_rec_bottom():
 
 	return bottom
 
+# get and clean housing data from City_Zhvi_AllHomes.csv
+def get_housing_data():
+	housing = pd.read_csv('City_Zhvi_AllHomes.csv')
+	housing.replace({'State':states}, inplace = True)
+
+	return housing
+# convert to quarters
+def convert_quarters():
+	housing = get_housing_data()
+
+	years = ["20{0:02d}".format(year) for year in range(17)]
+	year_month_pattern = re.compile('20\d\d-\d\d')
+	quarters = [re.compile("|".join(["{0:02d}".format(month) for month in range(start, start+3)])) for start in range(1,11,3)]
+	
+	all_years = housing.select(lambda x: year_month_pattern.match(x), axis = 1)
+	# print all_years
+	means = {}
+	for year_label in years:
+		year = all_years.select(lambda x: re.search(year_label, x), axis = 1)
+		for i, quarter_regex in enumerate(quarters):
+			quarter = year.select(lambda x: quarter_regex.search(x), axis = 1)
+			means["{0}q{1}".format(year_label, i+1)] = quarter.mean(axis = 1)
+	
+	quarters_in_data = pd.DataFrame(means).dropna(axis = 'columns', how = 'all')
+	
+	return quarters_in_data
+	# print housing.head()
+
+#  clean the housing Data
+def cnovert_housing_data_to_quarters():
+
+	housing = get_housing_data()
+	quarters = convert_quarters()
+	state_reg = [housing.State, housing.RegionName]
+	multi_index = pd.MultiIndex.from_tuples(list(zip(*state_reg)), names=["State","RegionName"])
+	housing = quarters.set_index(multi_index)
+
+	return housing
 
 
 university_towns_df = get_list_of_university_towns(university_towns_source)
 # print university_towns_df[university_towns_df.RegionName.str.startswith("Ypsilanti")].State.iloc[0]
 
-print get_rec_start()
-print get_rec_end()
-print get_rec_bottom()
+# print get_rec_start()
+# print get_rec_end()
+# print get_rec_bottom()
+
+print cnovert_housing_data_to_quarters().shape
